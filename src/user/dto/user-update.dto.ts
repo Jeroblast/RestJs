b969c92689e88df0306c1034a1dc62df9ede7580
jerom <jerome.cessat@gmail.com>
2024-05-15 15:39:49 +0200

    CRUD user 2

---
+++ src/user/dto/user-update.dto.ts
@@ -0,0 +1,7 @@
+export class UserUpdateDto {
+  firstname: string;
+  lastname: string;
+  password: string;
+  age: number;
+  birthcity: string;
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,19 +5,30 @@ import { CreateUserService } from "./usecase/create-user.service";
 import { UserController } from "./controller/user.controller";
 import { PasswordHasherService } from "./utils/password-hasher.service";
 import { PasswordHasherServiceInterface } from "./utils/password-hasher.service.interface";
+import { UpdateUserService } from "./usecase/update-user.service";
+import { GetAllUSersService } from "./usecase/get-all-users.service";
+import { GetUserByIdService } from "./usecase/get-user-by-id.service";
+import { GetUserByCityService } from "./usecase/get-user-city.service";
+import { UpdateUserPasswordService } from "./usecase/update-password-user.service";
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [PasswordHasherService,
-    {
-    provide: CreateUserService,
-    useFactory: (passwordHasherService: PasswordHasherServiceInterface) => {
-      return new CreateUserService(passwordHasherService);
-    },
-    inject: [PasswordHasherService],
-  },
+    CreateUserService,
+    UpdateUserService,
+    UpdateUserPasswordService,
+    GetAllUSersService,
+    GetUserByIdService,
+    GetUserByCityService
+    //{
+   // provide: CreateUserService,
+   // useFactory: (passwordHasherService: PasswordHasherServiceInterface) => {
+   //   return new CreateUserService(passwordHasherService);
+  //  },
+ //   inject: [PasswordHasherService],
+ // },
 
 ],
 })
